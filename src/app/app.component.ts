e5bb524d637f7f9ccdfebf2bc22169597968b88a
sr.bala <sr.bala.kkl@gmail.com>
2022-08-26 09:06:26 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {ChildComponent} from "./child/child.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'ViewChildAndAfterViewInit';
+
+  @ViewChild(ChildComponent) childData !: ChildComponent;
+
+  ngAfterViewInit():void{
+    console.log(this.childData.childValue1);//getting childValue1 from child component
+    console.log(this.childData.childValue1);//getting childValue2 from child component
+  }
+
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.sass']
+})
+export class ChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  childValue1 = 'sr';
+  childValue2 = 'bala';
+
+}
